1ce432628c4ec71467ec21c64e632444fdde1bf4
scottshowers <100591205+scottshowers@users.noreply.github.com>
2025-11-13 14:58:21 -0600

    Update __init__.py

--- utils/__init__.py
+++ utils/__init__.py
@@ -4,16 +4,17 @@ Utils package for XLR8
 
 from .secure_pdf_parser import (
     EnhancedPayrollParser,
+    SecurePayrollParser,
     PayrollFieldCategories,
     create_ukg_excel_export,
-    process_parsed_pdf_for_ukg,
-    SecurePayrollParser  # Backwards compatibility alias
+    process_parsed_pdf_for_ukg
 )
 
 __all__ = [
     'EnhancedPayrollParser',
+    'SecurePayrollParser',
     'PayrollFieldCategories',
     'create_ukg_excel_export',
-    'process_parsed_pdf_for_ukg',
-    'SecurePayrollParser'
+    'process_parsed_pdf_for_ukg'
 ]
+
