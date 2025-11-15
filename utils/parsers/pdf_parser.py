d7a1553c4be620f3a13a0bc6c8d78bbd6effcc57
scottshowers <100591205+scottshowers@users.noreply.github.com>
2025-11-15 10:28:10 -0600

    Create pdf_parser.py

---
+++ utils/parsers/pdf_parser.py
@@ -0,0 +1,14 @@
+"""
+PDF Parser Module - Stub for now
+Will be implemented with full parsing logic later
+"""
+
+class EnhancedPayrollParser:
+    """Placeholder parser class"""
+    
+    def __init__(self):
+        pass
+    
+    def parse(self, file):
+        """Stub parse method"""
+        return {"text": "", "error": "Parser not yet implemented"}
