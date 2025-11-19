5dca2d92c1b1919a469b39b8ec25a794a27f6b21
scottshowers <100591205+scottshowers@users.noreply.github.com>
2025-11-19 06:02:15 -0600

    Update and rename init.py to __init__.py

---
+++ utils/ai/__init__.py
@@ -0,0 +1,7 @@
+"""
+AI utilities module
+"""
+
+from .enhanced_llm_synthesizer import get_enhanced_synthesizer, EnhancedLLMSynthesizer
+
+__all__ = ['get_enhanced_synthesizer', 'EnhancedLLMSynthesizer']
