ff341b2e0b29c8d5d1e10b67b7052337a3ebf962
scottshowers <100591205+scottshowers@users.noreply.github.com>
2025-11-19 13:24:12 -0600

    Update __init__.py

--- utils/parsers/__init__.py
+++ utils/parsers/__init__.py
@@ -5,8 +5,8 @@ Intelligent PDF parsing with multi-stage fallback
 
 # Make modules available at package level
 from .dayforce_parser_enhanced import DayforceParserEnhanced, parse_dayforce_register
-from .intelligent_parser_orchestrator_enhanced import IntelligentParserOrchestrator, parse_pdf_intelligent
-from .intelligent_parser_ui_enhanced import IntelligentParserUI, render_intelligent_parser_ui
+from .intelligent_parser_orchestrator import IntelligentParserOrchestrator, parse_pdf_intelligent
+from .intelligent_parser_ui import IntelligentParserUI, render_intelligent_parser_ui
 
 __all__ = [
     'DayforceParserEnhanced',
