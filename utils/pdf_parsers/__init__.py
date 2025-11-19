c83c591ccc8fe7f104422f6e686d4f03702e1ed0
scottshowers <100591205+scottshowers@users.noreply.github.com>
2025-11-19 09:30:27 -0600

    Update __init__.py

--- utils/pdf_parsers/__init__.py
+++ utils/pdf_parsers/__init__.py
@@ -3,5 +3,11 @@ PDF Parser utilities for extracting structured data
 """
 
 from .register_extractor import PayrollPDFExtractor, extract_register_to_excel
+from .adaptive_register_parser import AdaptiveRegisterParser, extract_register_adaptive
 
-__all__ = ['PayrollPDFExtractor', 'extract_register_to_excel']
+__all__ = [
+    'PayrollPDFExtractor', 
+    'extract_register_to_excel',
+    'AdaptiveRegisterParser',
+    'extract_register_adaptive'
+]
