fc35945f4cf7abf9973203ab4e52bb566fa477a8
scottshowers <100591205+scottshowers@users.noreply.github.com>
2025-11-23 07:36:28 -0600

    Update api.js

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const api = axios.create({
-  baseURL: '/api',
+  baseURL: 'https://hcmpact-xlr8-production.up.railway.app/api',
   timeout: 60000,
   headers: {
     'Content-Type': 'application/json'
